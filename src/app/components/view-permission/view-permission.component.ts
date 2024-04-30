109af9a145f163723738c551e6498792a344b67a
uzair anwar <uzair.anwar@expertflow.com>
2024-04-30 18:11:26 +0500

    add permission view

--- src/app/components/view-permission/view-permission.component.ts
+++ src/app/components/view-permission/view-permission.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { API_ENDPOINTS } from 'src/app/const/api.config';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-view-permission',
@@ -7,4 +11,36 @@ import { Component } from '@angular/core';
 })
 export class ViewPermissionComponent {
 
+itemId?:string;
+  constructor(private httpService: HttpService,private route:ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.itemId = params['id'];
+      console.log("selected permission id : ",this.itemId);
+    this.getPermissionDetailsByIdV1();
+  });
+  }
+
+  getPermissionDetailsByIdV1() {
+    this.httpService.get<any>(`${API_ENDPOINTS.permissionV1}/${this.itemId}`)
+    .pipe(
+      catchError(error => {
+        console.error('Error in POST request:', error);
+        // Handle the error here or re-throw it to propagate
+        // return throwError(error); // Uncomment this line if you want to propagate the error
+        return throwError(() => error); // Or return a new observable with the error
+      })
+    )
+    .subscribe({
+      next: response => {
+        console.log('Response:', response);
+        //console.log('POST request successful:', this.permissions);
+      },
+      error: error => {
+        console.error('Error in POST request:', error);
+      }
+    });
+  }
 }
+
