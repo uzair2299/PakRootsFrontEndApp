3e99a5819793620be5a9049f147e658d91bcb425
uzair anwar <uzair.anwar@expertflow.com>
2024-07-23 14:35:36 +0500

    load menu on login

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-import { WebSocketService} from 'src/app/services/WebSocketService';
+import { DataService } from 'src/app/services/DataService';
+import { WebSocketService } from 'src/app/services/WebSocketService';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,13 +10,32 @@ import { WebSocketService} from 'src/app/services/WebSocketService';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent {
+  menu: any[] = [];
 
-  
-  constructor(private authService: AuthService, private router: Router,private webSocketService:WebSocketService) {}
+  constructor(private authService: AuthService, private router: Router, private webSocketService: WebSocketService, private dataService: DataService) { }
   logout(): void {
     this.authService.logout();
     this.webSocketService.disconnect();
     // Redirect to the login page or other desired action
-  this.router.navigate(['/login']);
+    this.router.navigate(['/login']);
   }
+
+
+  ngOnInit() {
+
+
+    this.dataService.menu$.subscribe(menuData => {
+      this.menu = menuData;
+      console.log(menuData)
+    });
+  }
+
+  navigateTo(child: any): void{
+    console.log(child.name);
+    if (child.name === 'Sign Out') {
+      this.logout();
+    }
+  }
+
+
 }
--- src/app/services/DataService.ts
+++ src/app/services/DataService.ts
@@ -4,13 +4,16 @@ import { BehaviorSubject, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
+
+//The issue of the menu options disappearing on page refresh is because the menu data is stored in memory (using BehaviorSubject), and it gets reset when the application reloads.
 export class DataService {
   private dataSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
   public data$: Observable<any> = this.dataSubject.asObservable();
 
 
-  private menuSource :  BehaviorSubject<any> = new BehaviorSubject<any[]>([]);
-  public menu$ = this.menuSource.asObservable();
+  private menuSource = new BehaviorSubject<any[]>(this.getMenuDataFromLocalStorage());
+  menu$ = this.menuSource.asObservable();
 
   emitData(data: any) {
     this.dataSubject.next(data);
@@ -18,7 +21,18 @@ export class DataService {
 
   setMenuData(menuData: any[]) {
     this.menuSource.next(menuData);
+    this.setMenuToLocalStorage(menuData);
+  }
+
+
+  private setMenuToLocalStorage(menuData: any[]) {
+    localStorage.setItem('menuData', JSON.stringify(menuData));
   }
 
   
+  private getMenuDataFromLocalStorage(): any[] {
+    const menuData = localStorage.getItem('menuData');
+    return menuData ? JSON.parse(menuData) : [];
+  }
+
 }
